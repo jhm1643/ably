47aa7b0b90ab36e9115ef493069cac7341acbca5
조하명 <>
2023-06-26 15:58:34 +0900

    1. README 내용 수정 2. 파일명 수정

--- src/main/java/com/ably/config/SpringdocConfig.java
+++ src/main/java/com/ably/config/SpringdocConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SpringdocOpenApiConfig {
+public class SpringdocConfig {
 
     @Bean
     public OpenAPI openAPI() {
